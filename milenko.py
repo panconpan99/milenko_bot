c6f63235b5f6a64db8886d0a6562693bb96cd641
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-02-18 12:10:37 -0300

    added commads

--- milenko.py
+++ milenko.py
@@ -1,14 +1,44 @@
 import os
 import discord 
 from dotenv import load_dotenv
+from discord import client, message, user
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
 
-bot = commands.Bot(command_prefix='#', help_command=None)
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
     print("listo")
+
+@bot.command()
+async def foo(ctx, arg):
+    print("prueba")
+    await ctx.send(arg)
+
+@bot.command()
+async def test(ctx):
+    print("entro")
+    await ctx.send("its works")
+
+@bot.event
+async def on_member_join(member):
+    await member.channel.send("bienvenido a este sv de prueba")
+
+@bot.event
+async def on_message(message):
+    if message.author == user :
+        return
+    if message.content == "hagalo usted mismo":
+        await message.channel.send("chao jasmina")
+    if message.content =="hola":
+        await message.channel.send("pati mi cola")
+    if message.content =="qlo":
+        await message.channel.send("voh mismo")
+    if message.content =="#dm":
+        await message.author.send( "funciono, a medias")
+
 
 
 load_dotenv()
