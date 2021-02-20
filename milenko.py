88bb988753efac75374658fae20a95f6382ce03d
panconpan99 <lucasrene.almonacid@alumnos.ulagos.cl>
2021-02-19 23:27:00 -0300

    added magicball

--- milenko.py
+++ milenko.py
@@ -5,6 +5,8 @@ import random
 import youtube_dl
 from discord.ext import commands
 from features.youtube_request import *
+from features.mensajes.message import MESSAGES
+from features.mensajes.lolball import eightball
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix = '!')
@@ -21,10 +23,8 @@ async def on_member_join(member):
 
 @bot.event
 async def on_command_error(ctx,error):
-    if isinstance(error,commands.MissingRequiredArgument):
-        await ctx.send("tonto")
-    if isinstance(error,commands.CommandNotFound):
-        await ctx.send("escribir bien no es tan dificil")
+    if isinstance(error,commands.MissingRequiredArgument) or isinstance(error,commands.CommandNotFound):
+        msg = await ctx.send(random.choice(MESSAGES))
 
 
 @bot.command(name= 'test', help = 'prueba de test')
@@ -35,10 +35,17 @@ async def test(ctx):
 async def hagalo_usted_mismo(ctx):
     await ctx.send("chao jasmina")
 
+#suerte
+
 @bot.command(help = "tira una moneda")
 async def coin(ctx,* ,msg):
     var=["verdadero","falso"]
     await ctx.send("{}".format(random.choice(var)))
+
+@bot.command(help = "prueba tu suerte")
+async def magicball(ctx,* ,msg):
+    choice = await ctx.send("la bola magica dice  '{}' ".format(random.choice(eightball)))
+
 
 #finder
 
